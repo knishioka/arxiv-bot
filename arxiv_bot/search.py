1092682a32f5f8a167b0af71e0668a39471165aa
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2021-09-09 07:52:48 +0900

    Parse search results

--- arxiv_bot/search.py
+++ arxiv_bot/search.py
@@ -1,7 +1,9 @@
 import feedparser
 
+from .article import Article
+
 
 def query():
     url = "http://export.arxiv.org/api/query?search_query=cat:cs.AI&start=0&max_results=5"
     data = feedparser.parse(url)
-    return data
+    return [Article(article_dict) for article_dict in data["entries"]]
